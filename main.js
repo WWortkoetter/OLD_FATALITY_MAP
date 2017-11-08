8cbb034f617096b571786a5a4b010e0f1d6fec91
WWortkoetter <32912778+WWortkoetter@users.noreply.github.com>
2017-11-08 13:36:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,15 +4,25 @@ var myFunctionHolder = {};
 
 //declaring function 1
 myFunctionHolder.addPopups = function (feature, layer) {
-    if (feature.properties && feature.properties.age) {
-    layer.bindPopup(
-        "<dl><dt>Location: </dt>" + feature.properties['trafid1']
-        + "<dt>Number Killed: </dt>" + feature.properties['numfatal']
-        + "<dt>Age: </dt>" + feature.properties['age']
-    );
+    var survived = '';
+    if (feature.properties['dthday'] == -1 || feature.properties['dthday'] == 88 || feature.properties['dthday'] == 99){
+        survived = 'Lived';
+    } 
+    else {
+        survived = 'Deceased';
+    }
+
+    if (feature.properties) {
+        layer.bindPopup(
+            "<dl><dt>Location: </dt>" + feature.properties['trafid1']
+            + "<dt>Number Killed: </dt>" + feature.properties['numfatal']
+            + "<dt>Age: </dt>" + feature.properties['age']
+            + "<dt>Year: </dt>" + feature.properties['caseyear']
+            + "<dt>Status: </dt>" + survived
+        );
     }
 }
-    
+
 //declaring function 2
 myFunctionHolder.pointToCircle = function (feature, latlng) {
     var geojsonMarkerOptions = {
@@ -21,11 +31,13 @@ myFunctionHolder.pointToCircle = function (feature, latlng) {
         color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: .8
     };
     var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
     return circleMarker;
 }
+
+
 
 //execute only when window is fully loaded
 window.onload = function () {
@@ -35,21 +47,45 @@ window.onload = function () {
         maxZoom: 18,
         attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
     }).addTo(mapObject);
-    
-    // fatalities is the variable name we defined in point data file. 
-    
+
+    // fatality points
     var fatalsLayerGroup = L.geoJSON(fatalities, {
         onEachFeature: myFunctionHolder.addPopups,
         pointToLayer: myFunctionHolder.pointToCircle
     });
-        
+
+    mapObject.addLayer(fatalsLayerGroup);
+    mapObject.fitBounds(fatalsLayerGroup.getBounds());
+
+    // clusters
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(fatalsLayerGroup);
+
+    // button to toggle clusters
+    var clustertoggle = document.getElementById("clusterToggle");
+    clustertoggle.onclick = function() {
+        if (!document.getElementById("unchecked2").checked){
+            mapObject.addLayer(clusters);
+            document.getElementById("unchecked2").checked = true;
+        }
+        else {
+            mapObject.removeLayer(clusters);
+            mapObject.removeLayer(fatalsLayerGroup);
+            mapObject.addLayer(fatalsLayerGroup);
+            document.getElementById("unchecked2").checked = false;
+        }
+    }
+
+    // heatmap
+
+    // heatmap config
     var cfg = {
         // radius should be small ONLY if scaleRadius is true (or small radius is intended)
         // if scaleRadius is false it will be the constant radius used in pixels
-        "radius": 0.015,
-        "maxOpacity": .8, 
+        "radius": .015,
+        "maxOpacity": .8,
         // scales the radius based on map zoom
-        "scaleRadius": true, 
+        "scaleRadius": true,
         // if set to false the heatmap uses the global maximum for colorization
         // if activated: uses the data maximum within the current map boundaries 
         //   (there will always be a red spot with useLocalExtremas true)
@@ -61,26 +97,22 @@ window.onload = function () {
         // which field name in your data represents the data value - default "value"
         valueField: 'value'
     };
-    
+
     var heatmapLayer = new HeatmapOverlay(cfg);
-    var heatmapLayerGroup = L.geoJSON().addLayer(heatmapLayer);
+    heatmapLayer.setData(fatalitiesHeatmapData);
 
-    var button = document.getElementById('heatmapToggle');
- 
-    mapObject.addLayer(fatalsLayerGroup);
-    mapObject.fitBounds(fatalsLayerGroup.getBounds());
-
-    if (document.getElementById("heatmapToggle").getAttribute("state") === 0){
-        print()
-        mapObject.removeLayer(heatmapLayerGroup);
-    }
-    else {
-        heatmapLayer.setData(fatalitiesHeatmapData);
-        heatmapLayerGroup = L.geoJSON().addLayer(heatmapLayer);
-        mapObject.addLayer(heatmapLayer);
+    // button to toggle heatmap
+    var heattoggle = document.getElementById("heatmapToggle");
+    heattoggle.onclick = function() {
+        if (!document.getElementById("unchecked1").checked){
+            mapObject.addLayer(heatmapLayer);
+            document.getElementById("unchecked1").checked = true;
+        }
+        else {
+            mapObject.removeLayer(heatmapLayer);
+            document.getElementById("unchecked1").checked = false;
+        }
     }
 
-    // var heatmapLayer = new HeatmapOverlay(cfg);
-    // heatmapLayer.setData(fatalitiesHeatmapData);
-    // mapObject.addLayer(heatmapLayer);
+    
 };
